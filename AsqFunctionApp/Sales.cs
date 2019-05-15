3952260763bf8b0afbc0ced27e5b0426b25a90ac
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-15 08:38:03 +0200

    Add collector access todo

--- AsqFunctionApp/Sales.cs
+++ AsqFunctionApp/Sales.cs
@@ -62,6 +62,7 @@ namespace AsqFunctionApp
         {
             context.GetLogger().LogInformation("Place order!");
             
+            //TODO: Do we force users to always use a string? If yes we can skip the generic on this method
             await context.GetAsyncCollector<string>()
                 .AddAsync("some-payload"); //push stuff out via native connectors
 
