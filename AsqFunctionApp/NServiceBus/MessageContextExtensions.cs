88f098a777da1a54bdf31f8787da626511c4c557
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-15 13:40:34 +0200

    No need to expose the logger

--- AsqFunctionApp/NServiceBus/MessageContextExtensions.cs
+++ AsqFunctionApp/NServiceBus/MessageContextExtensions.cs
@@ -2,18 +2,11 @@ using Microsoft.Azure.WebJobs;
 
 namespace NServiceBus
 {
-    using Microsoft.Extensions.Logging;
-
     static class MessageContextExtensions
     {
         public static IAsyncCollector<string> GetAsyncCollector(this IMessageHandlerContext context)
         {
             return context.Extensions.Get<IAsyncCollector<string>>();
-        }
-
-        public static ILogger GetLogger(this IMessageHandlerContext context)
-        {
-            return context.Extensions.Get<ILogger>();
         }
     }
 }
