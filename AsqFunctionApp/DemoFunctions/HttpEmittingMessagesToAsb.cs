2148e6b16d6b0d8dde874de204757fcf3db92996
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-16 12:53:45 +0200

    Add demo todos

--- AsqFunctionApp/DemoFunctions/HttpEmittingMessagesToAsb.cs
+++ AsqFunctionApp/DemoFunctions/HttpEmittingMessagesToAsb.cs
@@ -20,6 +20,11 @@ namespace FunctionApp
             endpoint.Routing.RouteToEndpoint(typeof(PlaceOrder), endpointName); //route to our self just to demo
         }
 
+        //TODO: Demos:
+        //  Show a saga
+        //  Show outbox deduplication via http header
+        //  Show error queue usage to not loose http requests
+        //  Show exposing a "http api" that does "1 to many"
         [FunctionName("placeOrder")]
         public static async Task<IActionResult> Run(
              [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
