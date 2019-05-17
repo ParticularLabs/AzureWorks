1fc633bd2239cf9f98652dfd07af4f3c3ff300d2
Sean Feldman <feldman.sean@gmail.com>
2019-05-17 14:00:57 -0600

    cleanup

--- AsqFunctionApp/DemoFunctions/AsqConnectedFunction.cs
+++ AsqFunctionApp/DemoFunctions/AsqConnectedFunction.cs
@@ -7,14 +7,13 @@ namespace FunctionApp
     using NServiceBus.AzureFuntions;
     using Microsoft.WindowsAzure.Storage.Queue;
 
-    //TODO
     public static class AsqConnectedFunction
     {
         static AsqConnectedFunction()
         {
             endpoint = new FunctionsAwareStorageQueueEndpoint(endpointName);
 
-            endpoint.Routing.RouteToEndpoint(typeof(SomeRoutedMessage), endpointName); //route to our self just to demo
+            endpoint.Routing.RouteToEndpoint(typeof(SomeRoutedMessage), endpointName); // route to our self just to demo
         }
 
         [FunctionName(endpointName)] // this is the "one function to all handlers for different messages" - A junction function
